8fb404b21173706e7b0e681b594458a9bd181391
AlvaroMontalvan <alvaro.montalvan@epn.edu.ec>
2024-07-03 15:22:54 -0500

    Factura

--- src/main/java/org/biblioteca/Factura.java
+++ src/main/java/org/biblioteca/Factura.java
@@ -1,3 +1,39 @@
-public class Factura {
+package org.biblioteca;
+import java.time.LocalDate;
 
+public class Factura {
+    private String nombreUsuario;
+    private LocalDate fecha;
+    private Float montoPagado;
+
+    public Factura(String nombreUsuario, LocalDate fecha, Float montoPagado) {
+        this.nombreUsuario = nombreUsuario;
+        this.fecha = fecha;
+        this.montoPagado = montoPagado;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public Float getMontoPagado() {
+        return montoPagado;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setMontoPagado(Float montoPagado) {
+        this.montoPagado = montoPagado;
+    }
 }
+
