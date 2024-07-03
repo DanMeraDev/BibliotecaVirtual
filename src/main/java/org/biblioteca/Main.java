7ec8f6db7064a8826873ed60d288dff922186eda
AlvaroMontalvan <alvaro.montalvan@epn.edu.ec>
2024-07-03 15:23:41 -0500

    Instanciar

--- src/main/java/org/biblioteca/Main.java
+++ src/main/java/org/biblioteca/Main.java
@@ -1,7 +1,11 @@
 package org.biblioteca;
-
+import org.biblioteca.modelos.Usuario;
+import org.biblioteca.modelos.Libro;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Usuario usuario = new Usuario();
+        Libro libro = new Libro();
+        
+
     }
 }
